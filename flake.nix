3694a69d58c57c8252f29e6985496bee32ea4210
Rob <67354942+DispatchCode3@users.noreply.github.com>
2026-03-09 00:07:16 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = inputs@{ self, nixpkgs, ... }:
     let
       lib = nixpkgs.lib;
 
@@ -16,6 +16,22 @@
 
       forAllSystems = f:
         lib.genAttrs supportedSystems (system: f system);
+
+      hostModules = {
+        desktop = ./modules/hosts/desktop.nix;
+        laptop = ./modules/hosts/laptop.nix;
+        vm = ./modules/hosts/vm.nix;
+        portable-usb = ./modules/hosts/portable-usb.nix;
+        server = ./modules/hosts/server.nix;
+      };
+
+      roleModules = {
+        minimal = ./modules/roles/minimal.nix;
+        workstation = ./modules/roles/workstation.nix;
+        dev = ./modules/roles/dev.nix;
+        gaming = ./modules/roles/gaming.nix;
+        vm-host = ./modules/roles/vm-host.nix;
+      };
 
       mkInstallerLib = system:
         let
@@ -33,13 +49,16 @@
             }:
             lib.nixosSystem {
               inherit system;
+
               specialArgs = {
-                inherit inputs self username host roles;
+                inherit inputs self hostname username host roles;
               };
+
               modules =
                 [
-                  ./modules/base.nix
-                  (./modules/hosts + "/${host}.nix")
+                  self.nixosModules.base
+                  hostModules.${host}
+
                   ({ ... }: {
                     networking.hostName = hostname;
 
@@ -51,31 +70,17 @@
                     system.stateVersion = stateVersion;
                   })
                 ]
-                ++ map (role: ./modules/roles/${role}.nix) roles
+                ++ map (role: roleModules.${role}) roles
                 ++ extraModules;
             };
         };
     in {
-      lib = forAllSystems (system: mkInstallerLib system);
+      lib = forAllSystems mkInstallerLib;
 
       nixosModules = {
         base = ./modules/base.nix;
-
-        hosts = {
-          desktop = ./modules/hosts/desktop.nix;
-          laptop = ./modules/hosts/laptop.nix;
-          vm = ./modules/hosts/vm.nix;
-          portable-usb = ./modules/hosts/portable-usb.nix;
-          server = ./modules/hosts/server.nix;
-        };
-
-        roles = {
-          minimal = ./modules/roles/minimal.nix;
-          workstation = ./modules/roles/workstation.nix;
-          dev = ./modules/roles/dev.nix;
-          gaming = ./modules/roles/gaming.nix;
-          vm-host = ./modules/roles/vm-host.nix;
-        };
+        hosts = hostModules;
+        roles = roleModules;
       };
 
       templates = {
