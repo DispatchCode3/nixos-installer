e1b9572bdacaa0e1cd7753f7e4df59b89b6ffe1b
Rob <67354942+DispatchCode3@users.noreply.github.com>
2026-03-09 00:09:35 -0500

    Update base.nix

--- modules/base.nix
+++ modules/base.nix
@@ -1,1 +1,15 @@
+{ pkgs, ... }:
 
+{
+  time.timeZone = "America/Chicago";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  networking.networkmanager.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    git
+    curl
+    wget
+    vim
+  ];
+}
